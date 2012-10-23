4c2aac3d729fcb77a7dac4fafb30a2c954ba93f5
Michael Haas <haas@computerlinguist.org>
2012-10-23 12:39:55 +0200

    Add description

--- README.md
+++ README.md
@@ -1,4 +1,17 @@
-leechi
+Website crawling utility
 ======
 
-Leechi
+Leechi is a robust and sneaky wrapper for urllib2.
+
+It is sneaky because it introduces
+random delays between requests
+and because it changes its User Agent string.
+
+It is robust because it will automatically retry failed requests.
+
+Additionally, it will handle cookies to make session handling
+easier or even automatic.
+
+Leechi development was sponsored by the Forschungsdaten Service Center
+of Universität Mannheim, Germany <http://service.informatik.uni-mannheim.de/>.
+
--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(name='Leechi',
       description='Website crawler utility',
       author='Michael Haas',
       author_email='haas@computerlinguist.org',
-      url='http://www.python.org/sigs/distutils-sig/',
+      url='https://github.com/mhaas/leechi',
       py_modules=['leechi'],
       classifiers = [ 'Development Status :: 4 - Beta',
                       'Intended Audience :: Developers',
@@ -17,4 +17,23 @@ setup(name='Leechi',
                       'Programming Language :: Python :: 2',
                       'Topic :: Internet :: WWW/HTTP',
                       'Topic :: Software Development :: Libraries'
+                    ],
+      long_description = """\
+Website crawling utility
+----------------------------
+
+Leechi is a robust and sneaky wrapper for urllib2.
+
+It is sneaky because it introduces
+random delays between requests
+and because it changes its User Agent string.
+
+It is robust because it will automatically retry failed requests.
+
+Additionally, it will handle cookies to make session handling
+easier or even automatic.
+
+Leechi development was sponsored by the Forschungsdaten Service Center
+of Universität Mannheim, Germany <http://service.informatik.uni-mannheim.de/>.
+"""
      )
